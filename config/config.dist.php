6c9584c0e86a72ef7cd8a3a47e32fa141f51774a
nmc <admin@nmc-lab.com>
2014-04-07 23:07:05 -0400

    Adding Stuff

---
+++ config/config.dist.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Main Config File.
+ */
+
+//Unique app ID.
+$app['key'] = "USE_A_UNIQUE_KEY_HERE";
+
+//Host URL
+$app['hostname'] = "nmc-lab.com";
+
+//Analytics info
+$app['webstats_ID'] = "XXX";
+$app['webstats_host'] = "XXX";
