4f23d0594d9387dab9249e7962697397812d96d2
nmc <admin@nmc-lab.com>
2014-04-13 16:50:14 -0400

    2014-04 initial article release.

---
+++ src/articles/2014/2014-04.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+    "2014-04-11" => array(
+        'title' => 'New version of the NMC-Lab Blog',
+        'id' => '1c345a8a',
+        'url' => 'new-version-nmc-lab-blog',
+        'htmlpage' => 'newNMCLabBlog.html.twig',
+        'date' => '2014-04-11',
+    ),
+    "2014-04-13" => array(
+        'title' => 'Ultimate Media Center for Storage,Gaming and Multimedia',
+        'id' => 'a5f4fc10',
+        'url' => 'ultimate-htpc-gaming-storage-multimedia',
+        'htmlpage' => 'HTPCSwordfish.html.twig',
+        'date' => '2014-04-13',
+    )
+);
